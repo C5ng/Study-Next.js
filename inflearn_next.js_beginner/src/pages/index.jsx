cc83121ad36d67ecf72e65718c166dc9b4fec964
공대표 <c5ngsdp@gmail.com>
2024-07-27 17:51:25 +0900

    Section 5. 실전 프로젝트로 배우는 React 기본 문법
    리액트 문법만으로 화면에 데이터 표시하기

--- inflearn_next.js_beginner/src/pages/index.jsx
+++ inflearn_next.js_beginner/src/pages/index.jsx
@@ -1,6 +1,18 @@
-// '/'에 해당하는 페이지 컴포넌트
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+// '/'에 해당하는 페이지 컴포넌트
 function ProductPage() {
+  const [products, setproducts] = useState();
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/products').then(response => {
+      setproducts(response.data);
+    })
+  }, []); // react 컴포넌트가 렌더링 되고 나서 바로 추가적으로 취할 동작들을 작성 가능하다.
+
+  console.log(products);
+
   return <div style={{ color: 'blue' }}>상품 목록 페이지</div>
 }
 
