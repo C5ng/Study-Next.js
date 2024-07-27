63d53571e82256940ba7be52ba3635cb6f3cfc2a
공대표 <c5ngsdp@gmail.com>
2024-07-27 17:58:29 +0900

    Section 5. 실전 프로젝트로 배우는 React 기본 문법
    useState와 useEffect 소개 및 상품 목록 표시

--- inflearn_next.js_beginner/src/pages/index.jsx
+++ inflearn_next.js_beginner/src/pages/index.jsx
@@ -11,9 +11,17 @@ function ProductPage() {
     })
   }, []); // react 컴포넌트가 렌더링 되고 나서 바로 추가적으로 취할 동작들을 작성 가능하다.
 
-  console.log(products);
-
-  return <div style={{ color: 'blue' }}>상품 목록 페이지</div>
+  return (
+    <div>
+      <h1>상품목록 페이지</h1>
+      <ul>
+        {products && 
+          products.map(product => {
+            return <li key={product.id}>{product.name}</li>
+        })}
+      </ul>
+    </div>
+  );
 }
 
 /**
